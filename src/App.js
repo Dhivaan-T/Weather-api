89efec74a8b95655a08bed8515e9be42cbdce17e
Dhivaan-T <youremail@example.com>
2024-11-06 23:20:04 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+//import Home from './Home'
+import Weather from './Weather'
+function App(){
+  return(
+    <div>
+      
+      { <Weather></Weather> }
+    </div>
+  )
+}
+
+export default App;
